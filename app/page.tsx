7056dd49667138d947498677fa8389443c0d1e44
Slowhand0309 <slowhand0309@gmail.com>
2023-05-15 19:08:49 +0900

    :construction: 長方形を追加できるようにした

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,16 @@
 
 import { useMap } from '../providers/liveblocks';
 
+const COLORS = ['#DC2626', '#D97706', '#059669', '#7C3AED', '#DB2777'];
+
+const getRandomInt = (max: number) => {
+  return Math.floor(Math.random() * max);
+};
+
+const getRandomColor = () => {
+  return COLORS[getRandomInt(COLORS.length)];
+};
+
 const Rectangle = ({ shape }: { shape: any }) => {
   const { x, y, fill } = shape;
 
@@ -17,12 +27,24 @@ const Rectangle = ({ shape }: { shape: any }) => {
 };
 
 const Canvas = ({ shapes }: { shapes: any }) => {
+  const insertRectangle = () => {
+    const shapeId = Date.now().toString();
+    const rectangle = {
+      x: getRandomInt(300),
+      y: getRandomInt(300),
+      fill: getRandomColor(),
+    };
+    shapes.set(shapeId, rectangle);
+  };
   return (
     <>
       <div className="canvas">
         {Array.from(shapes, ([shapeId, shape]) => {
           return <Rectangle key={shapeId} shape={shape} />;
         })}
+      </div>
+      <div className="toolbar">
+        <button onClick={insertRectangle}>Rectangle</button>
       </div>
     </>
   );
